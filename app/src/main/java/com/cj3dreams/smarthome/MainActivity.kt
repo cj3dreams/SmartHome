b8137a6f3009bcf777de91731b428e328fb807f7
3Dimensional <cj3dream@gmail.com>
2023-03-05 01:32:06 +0500

    Fixed & Realized many things
     - Completely transparent Status Bar
     - Added smooth transparency for background wallpaper of HomeFragment.kt
     - Fixed icons cuz of transparency in Figma, it was another color
     - Tab color selector realized as in Figma UI design
     - Many other things which I forgot, just check yourself :D

--- app/src/main/java/com/cj3dreams/smarthome/MainActivity.kt
+++ app/src/main/java/com/cj3dreams/smarthome/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.cj3dreams.smarthome
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 
@@ -13,9 +17,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
         navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+    }
+
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window
+        val winParams = win.attributes
+        if (on) winParams.flags = winParams.flags or bits
+         else winParams.flags = winParams.flags and bits.inv()
+        win.attributes = winParams
     }
 
 }
